1f2e628ef4f9273ea0180f5f0fded6bab62e1937
Narasimha <chennayagari.narasimha@gmail.com>
2026-02-17 17:20:59 +0530

    Phase 2 - MongoDB Atlas connected successfully

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb+srv://jobappuser:jobapp123@cluster0.klt2wax.mongodb.net/jobapp?appName=Cluster0")
+  .then(() => {
+    console.log("MongoDB Connected Successfully");
+  })
+  .catch((err) => {
+    console.error("MongoDB Connection Error:", err);
+  });
 
 app.use(cors());
 app.use(express.json());
