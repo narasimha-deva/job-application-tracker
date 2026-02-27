7fced3c8885a8f45e15236ab2b758595ec44fffe
Narasimha <chennayagari.narasimha@gmail.com>
2026-02-27 16:21:54 +0530

    Fix controller import final

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -6,7 +6,7 @@ const {
   getJobs,
   updateJob,
   deleteJob,
-} = require("../controllers/jobController");
+} = require("../controllers/jobcontroller");
 
 const protect = require("../middleware/authMiddleware");
 
